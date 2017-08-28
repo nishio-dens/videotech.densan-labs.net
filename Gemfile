54719c85b4e0a3a09e6fc3c72c02af4afe45aa73
Shinsuke Nishio <nishio@densan-labs.net>
2017-08-28 20:58:12 +0900

    article chroma subsampling

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem "middleman", "~> 4.1"
 gem "middleman-blog"
+gem "middleman-syntax"
 gem 'middleman-livereload'
 gem 'redcarpet', '~> 3.3', '>= 3.3.3'
 gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -23,6 +23,10 @@ Time.zone = 'Tokyo'
 # Helpers
 ###
 
+activate :syntax
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true, with_toc_data: true, tables: true, autolink: true, gh_blockcode: true
+
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "articles"
